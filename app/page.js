7bcef87c27986d326b985f5e304f5359500c87e3
bislij <funkybislij@gmail.com>
2024-03-25 21:26:15 -0500

    update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,32 @@
 import parse from 'html-react-parser';
 
+async function ArticleTitle({ name }) {
+  const apiUrl = `https://wiktionary.org/w/api.php?action=parse&format=json&page=${name}&formatversion=2`;
+
+  let title;
+
+  await fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        title = data.parse.title;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+  return ( 
+    <h1 className="text-slate-900 dark:text-white text-2xl font-medium tracking-tight mb-6">{title}</h1>
+  );
+}
+
+
 async function ArticleBody({ name }) {
-  const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&format=json&page=${name}&formatversion=2`;
+  const apiUrl = `https://wiktionary.org/w/api.php?action=parse&format=json&page=${name}&formatversion=2`;
 
   let rawPage;
 
@@ -13,14 +38,15 @@ async function ArticleBody({ name }) {
         return response.json();
     })
     .then(data => {
-        console.log(data.parse.text);
         rawPage = data.parse.text;
     })
     .catch(error => {
         console.error('Error:', error);
     });
 
-  return ( parse(rawPage) );
+  return ( 
+    parse(rawPage)
+  );
 }
 
 export default function Home() {
@@ -28,15 +54,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl md:container">
 
-        <h1 className="text-slate-900 dark:text-white text-2xl font-medium tracking-tight">Article Title</h1>
+        <h1 className="text-slate-900 dark:text-white text-4xl font-medium tracking-tight">Wikigrab</h1>
 
-        <p className="mt-5 first-line:uppercase first-line:tracking-widest
-        first-letter:text-7xl first-letter:font-bold first-letter:text-slate-900
-        first-letter:mr-3 first-letter:float-left">
-          
-        </p>
+      </div>
 
-        <ArticleBody name="test" />
+      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl md:container mt-6">
+
+        <ArticleTitle name="suit" />
+
+        <ArticleBody name="suit" />
 
       </div>
     </main>
